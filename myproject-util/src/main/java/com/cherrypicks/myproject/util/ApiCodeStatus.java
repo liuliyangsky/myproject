89ca8bd5d11da168eeee04c801476389dd827bd1
Sea liu <sealiu@cherrypicks.com>
2020-04-16 15:32:21 +0800

    alter init code

--- myproject-util/src/main/java/com/cherrypicks/myproject/util/ApiCodeStatus.java
+++
@@ -1,9 +0,0 @@
-package com.cherrypicks.myproject.util;
-
-public abstract class ApiCodeStatus {
-
-    public static final int SUCCUSS = 0;
-    public static final int FAILED = -1;
-
-    public static final int ILLEGAL_ARGUMENT = 10400;
-}
